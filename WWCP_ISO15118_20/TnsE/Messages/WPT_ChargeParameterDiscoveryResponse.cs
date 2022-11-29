bcf2b78a49293d637c9545205ced249ef49954df
Achim Friedland <achim.friedland@graphdefined.com>
2022-11-29 20:43:52 +0100

    Remaining official data structures implemented

---
+++ WWCP_ISO15118_20/TnsE/Messages/WPT_ChargeParameterDiscoveryResponse.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Copyright (c) 2021-2022 GraphDefined GmbH
+ * This file is part of WWCP ISO 15118-20 <https://github.com/OpenChargingCloud/WWCP_ISO15118_20>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using cloud.charging.open.protocols.ISO15118_20.V2gciCt;
+
+#endregion
+
+namespace cloud.charging.open.protocols.ISO15118_20.TnsE
+{
+    public class WPT_ChargeParameterDiscoveryResponse : ChargeParameterDiscoveryResponse
+    {
+
+        WPT_PowerClassTypes  PDInputPowerClass                      { get; }
+        RationalNumberType   SDMinOutputPower                       { get; }
+        RationalNumberType   SDMaxOutputPower                       { get; }
+        UInt16               SDMaxGroundClearanceSupport            { get; }
+        UInt16               SDMinGroundClearanceSupport            { get; }
+        RationalNumberType   PDMinCoilCurrent                       { get; }
+        RationalNumberType   PDMaxCoilCurrent                       { get; }
+        IEnumerable<Byte[]>  SDManufacturerSpecificDataContainer    { get; }
+
+
+    }
+
+}

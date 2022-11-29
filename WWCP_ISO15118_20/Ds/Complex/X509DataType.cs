c59e54b711b3f60d7c548ef978b72d8f06fc1d6d
Achim Friedland <achim.friedland@graphdefined.com>
2022-11-29 20:43:52 +0100

    Remaining official data structures implemented

---
+++ WWCP_ISO15118_20/Ds/Complex/X509DataType.cs
@@ -0,0 +1,40 @@
+﻿/*
+ * Copyright (c) 2021-2022 GraphDefined GmbH
+ * This file is part of WWCP ISO 15118-20 <https://github.com/OpenChargingCloud/WWCP_ISO15118_20>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System.Xml.Linq;
+
+#endregion
+
+namespace cloud.charging.open.protocols.ISO15118_20.Ds
+{
+
+    public class X509DataType
+    {
+
+        public X509IssuerSerialType?  X509IssuerSerial    { get; }
+        public Byte[]?                X509SKI             { get; }
+        public String?                X509SubjectName     { get; }
+        public Byte[]?                X509Certificate     { get; }
+        public Byte[]?                X509CRL             { get; }
+        public XElement?              AnyElement          { get; }
+
+
+    }
+
+}
---
+++ WWCP_ISO15118_20/Tns/Complex/Scheduled_AC_CLReqControlModeType.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Copyright (c) 2021-2022 GraphDefined GmbH
+ * This file is part of WWCP ISO 15118-20 <https://github.com/OpenChargingCloud/WWCP_ISO15118_20>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using cloud.charging.open.protocols.ISO15118_20.V2gciCt;
+
+#endregion
+
+namespace cloud.charging.open.protocols.ISO15118_20.Tns
+{
+
+    public class Scheduled_AC_CLReqControlModeType : Scheduled_CLReqControlModeType
+    {
+
+        public RationalNumberType?  EVMaximumChargePower         { get; }
+        public RationalNumberType?  EVMaximumChargePower_L2      { get; }
+        public RationalNumberType?  EVMaximumChargePower_L3      { get; }
+
+        public RationalNumberType?  EVMinimumChargePower         { get; }
+        public RationalNumberType?  EVMinimumChargePower_L2      { get; }
+        public RationalNumberType?  EVMinimumChargePower_L3      { get; }
+
+        public RationalNumberType   EVPresentActivePower         { get; }
+        public RationalNumberType?  EVPresentActivePower_L2      { get; }
+        public RationalNumberType?  EVPresentActivePower_L3      { get; }
+
+        public RationalNumberType?  EVPresentReactivePower       { get; }
+        public RationalNumberType?  EVPresentReactivePower_L2    { get; }
+        public RationalNumberType?  EVPresentReactivePower_L3    { get; }
+
+
+    }
+
+}

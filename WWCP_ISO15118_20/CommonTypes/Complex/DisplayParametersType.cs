8828da77eb43fa7e3055e4eab31158039473471c
Achim Friedland <achim.friedland@graphdefined.com>
2022-11-30 14:26:46 +0100

    Fix namespaces

---
+++ WWCP_ISO15118_20/CommonTypes/Complex/DisplayParametersType.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Copyright (c) 2021-2022 GraphDefined GmbH
+ * This file is part of WWCP ISO 15118-20 <https://github.com/OpenChargingCloud/WWCP_ISO15118_20>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace cloud.charging.open.protocols.ISO15118_20.CommonTypes
+{
+
+    public class DisplayParametersType
+    {
+
+        public PercentValueType?    PresentSOC                   { get; }
+        public PercentValueType?    MinimumSOC                   { get; }
+        public PercentValueType?    TargetSOC                    { get; }
+        public PercentValueType?    MaximumSOC                   { get; }
+        public UInt32?              RemainingTimeToMinimumSOC    { get; }
+        public UInt32?              RemainingTimeToTargetSOC     { get; }
+        public UInt32?              RemainingTimeToMaximumSOC    { get; }
+        public Boolean?             ChargingComplete             { get; }
+        public RationalNumberType?  BatteryEnergyCapacity        { get; }
+        public Boolean?             InletHot                     { get; }
+
+
+    }
+
+}
---
+++ WWCP_ISO15118_20/WPT/Messages/WPT_FinePositioningSetupResponse.cs
@@ -0,0 +1,39 @@
+﻿/*
+ * Copyright (c) 2021-2022 GraphDefined GmbH
+ * This file is part of WWCP ISO 15118-20 <https://github.com/OpenChargingCloud/WWCP_ISO15118_20>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#region Usings
+
+using cloud.charging.open.protocols.ISO15118_20.CommonTypes;
+
+#endregion
+
+namespace cloud.charging.open.protocols.ISO15118_20.WPT
+{
+    public class WPT_FinePositioningSetupResponse : AV2GResponse
+    {
+
+        public WPT_FinePositioningMethodListType  PrimaryDeviceFinePositioningMethodList    { get; }
+        public WPT_PairingMethodListType          PrimaryDevicePairingMethodList            { get; }
+        public WPT_AlignmentCheckMethodListType   PrimaryDeviceAlignmentCheckMethodList     { get; }
+        public UInt16                             NaturalOffset                             { get; }
+        public IEnumerable<Byte[]>                VendorSpecificDataContainer               { get; }
+        public WPT_LF_SystemSetupDataType         LF_SystemSetupData                        { get; }
+
+
+    }
+
+}
